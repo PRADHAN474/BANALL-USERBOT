2d40c0d413136b2066090f55e9801fc6fdb98f1d
OXYEGN <129831825+PRADHAN474@users.noreply.github.com>
2023-08-17 14:34:32 +0530

    Update mihir.py

--- mihir.py
+++ mihir.py
@@ -47,9 +47,53 @@ async def unban_user(app: Client, message: Message):
     except:
         await message.reply_text("**Usage:**\n`/unban [chat_id] [user_id]`")
 
+# New feature: Broadcast a message to all chats in CHATS
+@M.on_message(filters.user(SUDO_USERS) & filters.command('broadcast'))
+async def broadcast_message(_, message: Message):
+    text = message.text.split(' ', 1)[1]
+    for chat_id in CHATS:
+        await M.send_message(chat_id, text)
+
+# New feature: Pin a message in a chat
+@M.on_message(filters.user(SUDO_USERS) & filters.command('pin'))
+async def pin_message(_, message: Message):
+    chat_id = message.chat.id
+    if message.reply_to_message:
+        await message.reply_to_message.pin()
+        await message.reply_text("📌 **Message pinned successfully!**")
+    else:
+        await message.reply_text("**Please reply to a message to pin it.**")
+
+# New feature: Leave a chat
+@M.on_message(filters.user(SUDO_USERS) & filters.command('leave'))
+async def leave_chat(_, message: Message):
+    chat_id = message.chat.id
+    await M.leave_chat(chat_id)
+    await message.reply_text("👋 **Left the chat successfully!**")
+
+# New feature: Personal message to a user
+@M.on_message(filters.user(SUDO_USERS) & filters.command('pm'))
+async def send_personal_message(_, message: Message):
+    try:
+        user_id = int(message.text.split(' ', 2)[1])
+        text = message.text.split(' ', 2)[2]
+        await M.send_message(user_id, text)
+        await message.reply_text("💌 **Message sent successfully!**")
+    except:
+        await message.reply_text("**Usage:**\n`/pm [user_id] [message]`")
+
+# New feature: Delete all messages in a chat
+@M.on_message(filters.user(SUDO_USERS) & filters.command('deleteall'))
+async def delete_all_messages(_, message: Message):
+    chat_id = message.chat.id
+    async for msg in M.iter_history(chat_id):
+        await msg.delete()
+
 M.start()
 M.join_chat("BWANDARLOK")
 print("Bot Started Successfully")
 idle()
 M.stop()
+
+
 
